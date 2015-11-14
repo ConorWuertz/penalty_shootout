c08d7f540d0a2182ce6b0aee94b967b92f15326b
conorwuertz <conor.wuertz@gmail.com>
2015-11-14 01:29:33 -0500

    Added some key bindings

--- Shootout.py
+++ Shootout.py
@@ -6,14 +6,17 @@ class Shootout(object):
 
     def __init__(self, teamA, teamB):
  
-        self.teamA = Team(teamA) #create a team with the name of the first country
-        self.teamB = Team(teamB) #create a team with the name of the second country
+        self.teamA = Team(teamA)            #create a team with the name of the first country
+        self.teamB = Team(teamB)            #create a team with the name of the second country
         self.field = Field(teamA,teamB)     #create the field
-        self.referee = Referee() #create the referee
-        self.shootingTeam = teamA
-        self.goalieTeam = teamB
+        self.referee = Referee()            #create the referee
+        self.shootingTeam = self.teamA
+        self.goalieTeam = self.teamB
         root = Tk()
-        frame = Frame(root)
+        frame = Frame(root, width = 500, height = 500)
+        frame.bind("<Left>", self.leftKey)
+        frame.bind("<Right>", self.rightKey)
+        frame.focus_set()
         frame.pack()
 
         bottomframe = Frame(root)
@@ -30,18 +33,56 @@ class Shootout(object):
 
         blackbutton = Button(bottomframe, text="Black", fg="black")
         blackbutton.pack( side = BOTTOM)
-
+        self.playRound()
         root.mainloop()
 
-    def playRound(self, row, col):
+    def playRound(self):
 
-        wait()                  #wait for 3 seconds
-        shootingTeam.shoot()
-        goalieTeam.save()
+        #wait()                  #wait for 3 seconds
+        #self.shootingTeam.shoot()
+        #self.goalieTeam.save()
+        pass
 
-    def getpixel(self, row, col):
-        return self.data[row][col]
+    def leftKey(self,event):
+        print(self.shootingTeam.name + " shot left!")
 
-    def display(self):
-        print "\n".join(["".join(row) for row in self.data])
+    def rightKey(self,event):
+        print(self.shootingTeam.name + " shot right!")
+
+
+    def shotKeyDetected(self,event):
+        print(self.shootingTeam.name + " shot!")
+
+
+class Team:
+
+    def __init__(self, name):
+        self.name = name
+
+
+    def shoot(self):
+        #todo: fill in logic
+        print(self.name + " shoots!")
+
+    def save(self):
+        #todo: fill in logic
+        print(self.name + " saves!")
+
+
+
+
+
+class Referee:
+
+    def __init__(self):
+        pass
+
+
+class Field:
+
+    def __init__(self, teamA, teamB):
+        self.teamA = teamA
+        self.teamB = teamB
+
+
 
